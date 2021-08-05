1d5ada0bf8179ff3cf767089525fbf459d0f01a2
Mustafa Samet Can <88423566+MustafaSametCan@users.noreply.github.com>
2021-08-05 14:24:31 +0300

    Delete 1.Gün directory

--- 1.Gün/OndalıklıVeriTipleri.java
+++
@@ -1,7 +0,0 @@
-public class OndalıklıVeriTipleri {
-
-    public static void main(String[] args){
-        float sayi1= 34.1f;
-        System.out.println(sayi1);
-    }
-}
--- 1.Gün/SayısalVeriTipleri.java
+++
@@ -1,7 +0,0 @@
-public class SayısalVeriTipleri {
-    public static void main(String[] args){
-        int abc=333333128;
-        System.out.println(abc);
-    }
-
-}
