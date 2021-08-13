67578cdec18e7ea149de11cdc5ce01513fb8d422
Mustafa Samet Can <88423566+MustafaSametCan@users.noreply.github.com>
2021-08-13 13:23:09 +0300

    Practice2 added

---
+++ Day 8/Practice2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args){
+        int i,j,n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("The size you want to form the stars:");
+        n = input.nextInt();
+        for (i=1; i<=n; i++){
+            for(j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+            for(j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
