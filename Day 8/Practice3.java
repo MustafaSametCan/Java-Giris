ea3eb774119b19897de11639ce110067f1524d82
Mustafa Samet Can <88423566+MustafaSametCan@users.noreply.github.com>
2021-08-13 13:41:52 +0300

    Practice3 added

---
+++ Day 8/Practice3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Practice3 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num;
+        System.out.println("Please enter a number: ");
+        num=scan.nextInt();
+        int divisor=2;
+        int dividend=num;
+        System.out.println("Prime factors:");
+        while(divisor<num){
+            if(dividend%divisor==0){
+                System.out.print(divisor+" ");
+                dividend/=divisor;
+            }
+            else{
+                divisor++;
+            }
+        }
+        System.out.print(num == dividend ? "You enter a prime number":"" );
+
+
+    }
+}
